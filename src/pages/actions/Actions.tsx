cc68e602917888a29d3dcc4db9e3edf3496ce9d0
Guzmán Fernando Nahuel <nahuel1432@gmail.com>
2024-04-16 21:47:04 -0300

    .

--- src/pages/actions/Actions.tsx
+++ src/pages/actions/Actions.tsx
@@ -1,10 +1,11 @@
+import { CustomBreadcrumb } from "../../common/components/ui/CustomBreadcrumb";
 import { DashboardLayout } from "../../layout/DashboardLayout";
 
 const Actions = () => {
     return (
 
         <DashboardLayout>
-           
+            <CustomBreadcrumb />
         </DashboardLayout >
 
     )
