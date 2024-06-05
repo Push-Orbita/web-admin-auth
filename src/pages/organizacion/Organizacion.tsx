dcad2cf2162d11728e0ca9b22d07cb8da1b4ff3d
Guzmán Fernando Nahuel <nahuel1432@gmail.com>
2024-06-05 10:47:03 -0300

    add organization

---
+++ src/pages/organizacion/Organizacion.tsx
@@ -0,0 +1,11 @@
+import { DashboardLayout } from "../../layout/DashboardLayout"
+
+const Organizacion = () => {
+    return (
+        <DashboardLayout>
+            <div>asd</div>
+        </DashboardLayout>
+    )
+}
+
+export default Organizacion
