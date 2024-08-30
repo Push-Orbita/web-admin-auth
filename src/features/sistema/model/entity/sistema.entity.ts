60a9263f0d89685c0e87aa92542061172b63dffb
Guzmán Fernando Nahuel <nahuel1432@gmail.com>
2024-08-30 17:50:39 -0300

    add custom table extended

--- src/features/sistema/model/entity/sistema.entity.ts
+++ src/features/sistema/model/entity/sistema.entity.ts
@@ -5,10 +5,32 @@ export interface SistemaResponse {
     metadata: MetadataEntity;
 }
 
+
 export interface SistemaEntity {
-    id: number;
-    nombre?: string;
+    id:            number;
+    nombre:        string;
+    descripcion:   string;
+    url:           string;
+    clientId:      string;
+    clientSecret:  string;
+    icono:         string;
+    suscripciones?: Suscripcion[] | [] | null;
+    modulos?:       Modulo[] | [] | null ;
+    permisos?:      any[] | [] | null;
+}
+
+export interface Modulo {
+    id:          number;
+    nombre:      string;
+    descripcion: string;
+    label:       string;
+    element:     string;
+    icon:        string;
+    path:        string;
+}
+
+export interface Suscripcion {
+    id:          number;
+    nombre?:      string;
     descripcion?: string;
-    url?: string,
-    icono?: string
 }
