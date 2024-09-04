a0bb571511762c7227895f5928244bf10f103cb2
Guzmán Fernando Nahuel <nahuel1432@gmail.com>
2024-09-04 19:42:23 -0300

    Update FormFields.tsx

--- src/features/usuario/components/form/FormFields.tsx
+++ src/features/usuario/components/form/FormFields.tsx
@@ -9,6 +9,7 @@ import { FormSelect } from "@components/common/forms/FormSelect";
 import useQueryApi from "@hooks/useQueryApi";
 import { PersonaEntity } from "@features/persona/model/entity/persona.entity";
 import { PersonaApi } from "@features/persona/service/persona.service";
+import { Fieldset } from "primereact/fieldset";
 
 const FormFields: FC = () => {
     const { handleSubmit } = useFormikContext<UsuarioPostDTO>();
@@ -26,30 +27,36 @@ const FormFields: FC = () => {
     }, [data]);
     return (
         <Form onSubmit={handleSubmit}>
-            <div className="p-fluid formgrid grid mb-3">
-                <div className="col-12 md:col-6 lg:col-6">
-                    <FormTextInput label={t(lang.User.form.name)} name={'nombre'} />
-                </div>
-                <div className="col-12 md:col-6 lg:col-6">
-                    <FormTextInput label={t(lang.User.form.email)} name={'email'} />
-                </div>
-                <div className="col-12 md:col-6 lg:col-6 mt-2">
-                    <FormTextInput label={t(lang.User.form.password)} name={'password'} type="password" />
-                </div>
-                <div className="col-12 md:col-6 lg:col-6 mt-2">
-                    <FormTextInput label={t(lang.User.form.repeatPassword)} name={'repeatPassword'} type="password" />
-                </div>
-                <div className="col-12 md:col-6 lg:col-6 mt-2">
-                    <FormSelect
-                        label="personas"
-                        name="persona"
-                        options={personaOptions}
-                        optionLabel="nombre"
-                        isLoading={isLoading}
+            <Fieldset legend="Persona" className=" mt-3 mb-3">
+                <div className="p-fluid formgrid grid mb-3">
+                    <div className="col-12 md:col-6 lg:col-6 mt-2">
+                        <FormSelect
+                            label={t(lang.User.form.person)}
+                            name="persona"
+                            options={personaOptions}
+                            optionLabel="nombre"
+                            isLoading={isLoading}
 
-                    />
+                        />
+                    </div>
                 </div>
-            </div>
+            </Fieldset>
+            <Fieldset legend="Datos Del Usuario" className=" mt-3 mb-3">
+                <div className="p-fluid formgrid grid mb-3">
+                    <div className="col-12 md:col-6 lg:col-6">
+                        <FormTextInput label={t(lang.User.form.name)} name={'nombre'} />
+                    </div>
+                    <div className="col-12 md:col-6 lg:col-6">
+                        <FormTextInput label={t(lang.User.form.email)} name={'email'} />
+                    </div>
+                    <div className="col-12 md:col-6 lg:col-6 mt-2">
+                        <FormTextInput label={t(lang.User.form.password)} name={'password'} type="password" />
+                    </div>
+                    <div className="col-12 md:col-6 lg:col-6 mt-2">
+                        <FormTextInput label={t(lang.User.form.repeatPassword)} name={'repeatPassword'} type="password" />
+                    </div>
+                </div>
+            </Fieldset>
             <FormCustomButtons />
         </Form>
     );
